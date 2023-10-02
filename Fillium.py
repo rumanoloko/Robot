773299415438adff79dc1c9dbeb65eed6a5a3ef5
Asgqb <ansersaugar@outlook.es>
2023-10-02 17:17:00 +0200

    Prueba

--- Fillium.py
+++
@@ -1,9 +0,0 @@
-from Pater import Pater
-
-
-class Fillium(Pater):
-    def __init__(self):
-        super().__init__()
-
-    def methodus_pro_populo(self):
-        print("Ego sum fillius")
--- alterum.py
+++
@@ -1,7 +0,0 @@
-
-class Robot:
-    def __int__(self):
-        pass
-
-    def metodo(self):
-        return 0
